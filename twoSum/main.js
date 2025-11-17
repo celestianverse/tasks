44d888e0ad767c7c615f13d9fea18d84298c7b05
Celestian <celestian.verse@gmail.com>
2025-11-17 22:26:30 +0300

    feat: solve Two Sum in JavaScript

---
+++ twoSum/main.js
@@ -0,0 +1,42 @@
+const twoSumForFor = (nums, target) => {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+};
+
+const twoSumMap = (nums, target) => {
+    const storage = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        const diff = target - num;
+
+        if (storage.has(diff)) {
+            return [storage.get(diff), i];
+        }
+
+        storage.set(num, i);
+    }
+};
+
+const twoSumObject = (nums, target) => {
+    const storage = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        const diff = target - num;
+
+        if (storage[diff] !== undefined) {
+            return [storage[diff], i];
+        }
+
+        storage[num] = i;
+    }
+};
+
+const result = twoSumForFor([10, 4, 0, 5, 6], 10);
+console.log(result);
